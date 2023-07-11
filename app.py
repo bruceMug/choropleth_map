bb3e2dd826ad483744320ec699e188b9227a8b94
brucemug <mugzbru199@gmail.com>
2023-07-11 10:20:47 +0300

    used list compression instead of for loop

--- app.py
+++ app.py
@@ -4,11 +4,16 @@ import geojson
 from shapely import wkb
 import config
 
+from timeit import default_timer as timer           # for timing the code
+
 app = Flask(__name__)
 
+# precompile wkb loading function
+load_wkb = wkb.loads
 
 def to_geojson(parish, pm2_5, wkb_string, id):
-    geometry = wkb.loads(bytes.fromhex(wkb_string))
+    # geometry = wkb.loads(bytes.fromhex(wkb_string))
+    geometry = load_wkb(wkb_string)
     feature = geojson.Feature(id=str(id), geometry=geometry, properties={
                               "parish": parish, "pm2_5": pm2_5})
     return feature
@@ -26,30 +31,41 @@ def get_map():
 
     # create cursor object
     cur = conn.cursor()
+    start = timer()                 # start timer   # todo : remove this
     cur.execute('''SELECT parish, pm2_5, geometry FROM airqo_data''')
-    # cur.execute('''SELECT parish, population_density FROM airqo_data''')
+    
     rows = cur.fetchall()  # fetch all rows
 
-    # print(len(rows))
+    end = timer()                                                       # end timer     # todo : remove this
+    print("Fetch row time: ", end - start)                            # todo : remove this
+    
+    start = timer()                                                     # start timer   # todo : remove this
+    """
     features = []
     i = 0
     for row in rows:
-        if i < 2000:
+        if i < 1000:
             parish = row[0]
             pm2_5 = row[1]
             geometry = row[2]
             i = i+1
-            features.append(to_geojson(parish, round(pm2_5), geometry, id=i))
-
+            features.append(to_geojson(parish, round(pm2_5), geometry, id=i)) """
+    
+    features = [to_geojson(row[0], round(row[1]), row[2], id=i) for i, row in enumerate(rows) if i < 1000]
+    
+    end = timer()                                                      # end timer     # todo : remove this
+    print("Geojson collection time: ", end - start)                              # todo : remove this
+    
     feature_collection = geojson.FeatureCollection(features)
-
-    # f = open('parish_data_5000.json', 'w')
+    # print(feature_collection)
+    # todo : check the features of all the rows
+    
+    # f = open('parish_data_2000.json', 'w')
     # f.write(feature_collection.__str__())
     # f.close()
 
     cur.close()
     conn.close()
-    # print(feature_collection)
     return jsonify(feature_collection)
 
 
